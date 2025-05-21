a8362e6101014252da6d553c401b812c4364bb50
Matt <84277551+MattRueter@users.noreply.github.com>
2025-05-21 13:44:57 -0500

    add morgan

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const fs = require('fs');
 const Database = require('better-sqlite3');
 const cors = require('cors');
+const morgan = require('morgan');
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,6 +13,7 @@ const DB_FILE = './notes.db';
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan('tiny'));
 
 // Initialize or connect to DB
 const db = new Database(DB_FILE);
